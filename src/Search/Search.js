09b290c4f91c282a7a8a9b5083a00a77db581eba
manvith22 <chekurthimanvith32@gmail.com>
2021-07-02 20:44:27 +0530

    Search Page

---
+++ src/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavBar} from '../NavBar/NavBar';
+import {SubNav} from '../NavBar/SubNav/SubNav';
+export function Search() {
+    return (
+        <div>
+            <NavBar/>
+            <SubNav/>
+        </div>
+    );
+}
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
 
-export function SearchBar() {
+export function SearchBar(props) {
+    const sizeClass = props.small ? '' : 'is-medium'
     return(
         <div>
             <div className="field has-addons">
                <p className="control">
-                   <button className="button is-static is-medium">Search</button>
+                   <button className={`button is-static ${sizeClass}`}>Search</button>
                </p>
                <p className="control">
-                  <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="burgers,spas,barber,bars"/>
+                  <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="burgers,spas,barber,bars"/>
                </p>
                <p className="control">
-                   <button className="button is-static is-medium">Near</button>
+                   <button className={`button is-static ${sizeClass}`}>Near</button>
                </p>
                <p className="control">
-                  <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="where"/>
+                  <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="where"/>
                </p>
-               <button className={`button is-medium ${styles['search-bar']}`}>
+               <button className={`button ${sizeClass} ${styles['search-bar']}`}>
                  <span className={`icon is-small ${styles['search-icon']}`}>
                    <i className="fas fa-search"></i>
                  </span>
